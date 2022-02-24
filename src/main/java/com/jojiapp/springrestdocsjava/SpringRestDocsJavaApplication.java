4aab7248d2100dc77802a9d0a2b1844715d8ca72
조지헌 <jojiapp@gmail.com>
2022-02-25 00:07:56 +0900

    init

---
+++ src/main/java/com/jojiapp/springrestdocsjava/SpringRestDocsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.jojiapp.springrestdocsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDocsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestDocsJavaApplication.class, args);
+	}
+
+}
